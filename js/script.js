eb5aab514b9314acb5ff16e76b1829ab8107087d
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-17 14:32:52 +0300

    b

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-//var array = [];
+var array = [];
 
 //Алгоритм, который выводит 9 цифр первые, которые равны 1, а остальные 0. 
-//for(var i = 0; i < 10; i++) {
-    //if (i == 0 || i == 9) {
-        //array[i] = 1;
-    //} else {
-        //array[i] = 0;
-    //}
-//}
+for(var i = 0; i < 10; i++) {
+    if (i == 0 || i == 9) {
+        array[i] = 1;
+    } else {
+        array[i] = 0;
+    }
+}
+
+console.log(array);
+
+var b = [];
 
 //Этот алгоритм будет выводить числа 1 и 0 через одного.
-//for(var i = 0; i < 9; i++) {
-    //if (i % 2 == 0) {
-        //array[i] = 0;
-    //} else {
-        //array[i] = 1;
-    //}
-//};
+for(var i = 0; i < 9; i++) {
+    if (i % 2 == 0) {
+        b[i] = 0;
+    } else {
+        b[i] = 1;
+    }
+};
 
-//console.log(array);
+console.log(b);
